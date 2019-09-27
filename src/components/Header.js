960048199872868bf5255090139e29825eedaae7
Tafang Joshua <tafodinho@gmail.com>
2019-09-27 08:52:11 +0100

    fixed linting issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,20 @@
 import React from 'react';
 
-const Header = (props) => {
-
-  return (
-    <div className="header">
-      <div className="container inner-header">
-        <ul>
-          <li className="logo">Bookstore CMS</li>
-          <li className="header-text">BOOKS</li>
-          <li className="header-text active">CATEGORIES</li>
-          <li className="user-profile clearfix">
-            <div className="oval">
-              <i class="fas fa-user-alt"></i>
-            </div>
-          </li>
-        </ul>
-      </div>
+const Header = () => (
+  <div className="header">
+    <div className="container inner-header">
+      <ul>
+        <li className="logo">Bookstore CMS</li>
+        <li className="header-text">BOOKS</li>
+        <li className="header-text active">CATEGORIES</li>
+        <li className="user-profile clearfix">
+          <div className="oval">
+            <i className="fas fa-user-alt" />
+          </div>
+        </li>
+      </ul>
     </div>
-  );
-};
+  </div>
+);
 
 export default Header;
